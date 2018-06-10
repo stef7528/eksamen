9eef824ce46779a08fd78433160d248d4ef56763
Stefani Arsovic <stefaniarsovic@Stefanis-MacBook-Pro.local>
2018-06-10 22:19:01 +0200

    færdig

--- java.js
+++ java.js
@@ -1,5 +1,8 @@
 var slideIndex = 0;
-carousel();
+if ($(".autoslide").length > 0) {
+    carousel();
+}
+
 
 function carousel() {
     var i;
@@ -13,4 +16,25 @@ function carousel() {
     }
     x[slideIndex - 1].style.display = "block";
     setTimeout(carousel, 2000);
+}
+
+
+
+$(document).ready(moveIn);
+
+var antalPoser = 0;
+$("#slutning").hide();
+
+
+function moveIn() {
+
+    console.log("moveIn");
+    $("#skyer").show();
+    $("#lilly_container").addClass("lilly_move_in");
+    $("#lilly_sprite").addClass("lilly_flyver");
+    $("#ella_container").addClass("ella_move_in");
+
+
+    $("#ella_sprite").addClass("ella_flyver");
+
 }
